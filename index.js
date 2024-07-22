537f24454e2d1e0142f5e04b371834e7959f1350
David Nitu <davidnitu44@gmail.com>
2024-07-22 21:37:00 +0100

    request body parsing setup for json and urlenconded

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const express = require('express')
+const express = require('express');
 
 const server = express();
 
-server.listen(3000, () => {
-  console.log('ECOMM Server Status: Live')
-})
+server.use(express.urlencoded({extended: true}))
+server.use(express.json())
+
+server.get('/', (req, res) => {
+  res.send(`
+    <div>
+      <form method="POST">
+        <input name="email" placeholder="E-mail"/>
+        <input type="password"  name="password" placeholder="Password"/>
+        <input type="password" name="passwordConfirm"  placeholder="Confirm Password"/>
+        <button>Submit</button>
+      </form>
+    </div>
+  `);
+});
+
+server.post('/', (req, res) => {
+  res.send('authenticated');
+});
+
+server.listen(3001, () => {
+  console.log('ECOMM Server Status: Live (PORT: 3001)');
+});
