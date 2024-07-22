69268c78f24cc596218799a65cc563244a6fbc23
David Nitu <davidnitu44@gmail.com>
2024-07-22 22:14:09 +0100

    implemented getAll method to retrieve all users in the respective repository

--- repositories/users.js
+++ repositories/users.js
@@ -5,15 +5,31 @@ class UsersRepository {
     if (!filename) {
       throw new Error('Creating a repository requires a file name');
     }
-
     this.filename = filename;
 
+    // check if file exists and create file otherwise
     try {
       fs.accessSync(this.filename);
     } catch (error) {
       fs.writeFileSync(this.filename, '[]');
     }
   }
+
+  async getAll() {
+    // open file, parse and read contents
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8',
+      })
+    );
+  }
 }
 
-const repo = new UsersRepository('users.json');
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+
+  const users = await repo.getAll();
+  console.log(users);
+};
+
+test();
