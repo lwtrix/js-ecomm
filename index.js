f837338c0cb94698191f9a7dae3f9b026cab5bd0
David Nitu <davidnitu44@gmail.com>
2024-07-25 22:42:35 +0100

    added basic signin GET route

--- index.js
+++ index.js
@@ -1,53 +1,69 @@
 const express = require('express');
-const cookieSession = require('cookie-session')
-const Users = require('./repositories/users.js')
+const cookieSession = require('cookie-session');
+const Users = require('./repositories/users.js');
 
 const server = express();
 
-server.use(express.urlencoded({extended: true}))
-server.use(express.json())
-server.use(cookieSession({
-  keys: ['adwfrwfdawd21d2']
-}))
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(
+  cookieSession({
+    keys: ['adwfrwfdawd21d2'],
+  })
+);
 
 server.get('/signup', (req, res) => {
   res.send(`
     <div>
+    <h1>Create an account</h1>
     You're signed in as: ${req.session.user}
       <form method="POST">
         <input name="email" placeholder="E-mail"/>
         <input type="password"  name="password" placeholder="Password"/>
         <input type="password" name="passwordConfirmation"  placeholder="Confirm Password"/>
-        <button>Submit</button>
+        <button>Sign Up</button>
       </form>
     </div>
   `);
 });
 
 server.post('/signup', async (req, res) => {
-  const { email, password, passwordConfirmation} = req.body
+  const { email, password, passwordConfirmation } = req.body;
 
-  const userExists = await Users.getOneBy({ email })
+  const userExists = await Users.getOneBy({ email });
 
-  if(userExists) {
-    return res.send('Email is in use')
+  if (userExists) {
+    return res.send('Email is in use');
   }
 
-  if(password !== passwordConfirmation) {
-    return res.send('Password and password confirmation must match')
+  if (password !== passwordConfirmation) {
+    return res.send('Password and password confirmation must match');
   }
 
-  const user = await Users.create({email, password})
-  req.session.user = user
+  const user = await Users.create({ email, password });
+  req.session.user = user;
 
   res.send('authenticated');
 });
 
-server.get('/signout', (req, res) => {
-  req.session = null
+server.get('signin', (req, res) => {
+  res.send(`
+    <div>
+    <h1>Sign in</h1>
+      <form method="POST">
+        <input name="email" placeholder="E-mail"/>
+        <input type="password"  name="password" placeholder="Password"/>
+        <button>Sign In</button>
+      </form>
+    </div>
+  `);
+});
 
-  res.redirect('/signup')
-})
+server.get('/signout', (req, res) => {
+  req.session = null;
+
+  res.redirect('/signin');
+});
 
 server.listen(3001, () => {
   console.log('ECOMM Server Status: Live (PORT: 3001)');
