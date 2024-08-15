afc1a69afc7c6299d81a27e46d3ad807a7584d0b
David Nitu <davidnitu44@gmail.com>
2024-08-15 21:29:01 +0100

    added styling for admin signin page

--- views/admin/auth/signin.js
+++ views/admin/auth/signin.js
@@ -4,14 +4,31 @@ const getError = require('../../utils.js')
 module.exports = ({ errors }) => {
   return layout({
     content: `
-    <div>
-    <h1>Sign in</h1>
+    <div class="col-lg-4 col-12 mx-auto mt-5">
+      <h1>Sign in</h1>
       <form method="POST">
-        <input name="email" placeholder="E-mail"/>
-        ${getError(errors, 'email')}
-        <input type="password"  name="password" placeholder="Password"/>
-        ${getError(errors, 'password')}
-        <button>Sign In</button>
+
+        <div class="mb-2 mt-4">
+          <label for="emailField" class="form-label">Email</label>
+            <div class="input-group has-validation">
+              <input name="email" type="text" class="form-control ${getError(errors, 'email') ? "is-invalid" : ""}" id="emailField" aria-describedby="emailFieldFeedback">
+              <div id="emailFieldFeedback" class="invalid-feedback">
+                ${getError(errors, 'email')}
+              </div>
+            </div>
+        </div>
+
+        <div class="mb-3">
+          <label for="passwordField" class="form-label">Password</label>
+            <div class="input-group has-validation">
+              <input name="password" type="password" class="form-control ${getError(errors, 'password') ? "is-invalid" : ""}" id="passwordField" aria-describedby="passwordFieldFeedback">
+              <div id="passwordFieldFeedback" class="invalid-feedback">
+                ${getError(errors, 'password')}
+              </div>
+            </div>
+        </div>
+
+        <button class="btn btn-primary col-4">Sign In</button>
       </form>
     </div>
   `,
