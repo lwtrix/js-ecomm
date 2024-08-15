0e8fa85e91ac9ac1e54934f221a6418f9df5de22
David Nitu <davidnitu44@gmail.com>
2024-08-15 19:02:44 +0100

    added styling for admin add product page + validation feedback

--- views/admin/products/add.js
+++ views/admin/products/add.js
@@ -4,14 +4,40 @@ const layout = require('../layout');
 module.exports = ({ errors }) => {
   return layout({
     content: `
-      <div>
-        <form method="POST" enctype="multipart/form-data">
-          <input placeholder="Name" name="productName"/>
-          ${getError(errors, 'productName')}
-          <input placeholder="Price" name="productPrice"/>
-          ${getError(errors, 'productPrice')}
-          <input type="file" name="productImage"/>
-          <button>Submit</button>
+      <div class="container mt-5">
+        <form method="POST" enctype="multipart/form-data" class="col-lg-6 col-12 mx-auto">
+
+          <div class="mb-2">
+            <label for="productNameField" class="form-label">Product Name</label>
+            <div class="input-group has-validation">
+              <input name="productName" type="text" class="form-control ${getError(errors, 'productName') ? "is-invalid" : ""}" id="productNameField" aria-describedby="productNameFieldFeedback">
+              <div id="productNameFieldFeedback" class="invalid-feedback">
+                ${getError(errors, 'productName')}
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-2">
+            <label for="productPriceField" class="form-label">Price</label>
+            <div class="input-group has-validation">
+              <input name="productPrice" type="text" class="form-control ${getError(errors, 'productPrice') ? "is-invalid" : ""}" id="productPriceField" aria-describedby="productPriceFieldFeedback">
+              <div id="productPriceFieldFeedback" class="invalid-feedback">
+                ${getError(errors, 'productPrice')}
+              </div>
+            </div>
+          </div>
+
+          <div class="mb-4">
+            <label for="productImageField" class="form-label">Thumbnail</label>
+            <div class="input-group has-validation">
+              <input name="productImage" type="file" class="form-control" id="productImageField" aria-describedby="productImageFieldFeedback">
+              <div id="productImageFieldFeedback" class="invalid-feedback">
+                Validation error
+              </div>
+            </div>
+          </div>
+
+          <button class="btn btn-success col-2">Publish</button>
         </form>
       </div>
     `,
