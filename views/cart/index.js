c87feb72d32213ba855efa1b99858167453de390
David Nitu <davidnitu44@gmail.com>
2024-08-27 18:35:47 +0100

    improved cart view item entry row alignment

--- views/cart/index.js
+++ views/cart/index.js
@@ -18,27 +18,35 @@ module.exports = ({ items }) => {
               item.productImage
             }" class="item-img">
           </th>
-          <td><strong>${item.productName}</strong></td>
-          <td>£${item.productPrice * item.quantity}</td>
           <td>
-            <div class="d-flex align-items-center">
+            <div class="mt-3">
+              <strong>${item.productName}</strong>
+            </div>
+          </td>
+          <td>
+            <div class="mt-3">
+              £${item.productPrice * item.quantity}
+            </div>
+          </td>
+          <td>
+            <div class="d-flex align-items-center mt-3">
               <form method="POST" action="/cart/${item.id}/increase">
-                <button class="btn">
+                <button class="btn m-0 p-0 mx-1 arrow-btn">
                   <i class="bi bi-caret-left-fill"></i>
                 </button>
               </form>  
               <span class="mx-2">${item.quantity}</span>
               <form method="POST" action="/cart/${item.id}/decrease">
-                <button  class="btn" ${item.quantity < 2 ? 'disabled' : ''}>
+                <button  class="btn m-0 p-0 mx-1" ${item.quantity < 2 ? 'disabled' : ''}>
                   <i class="bi bi-caret-right-fill"></i>
                 </button>
               </form>
             </div>
           </td>
           <td>
-          <form method="POST" action="/cart/${item.id}/delete">
-            <button class="btn btn-danger">Delete</button>
-          </form>
+            <form method="POST" action="/cart/${item.id}/delete" class="mt-3">
+              <button class="btn btn-danger">Delete</button>
+            </form>
           </td>
         </tr>
       `
