f39eff22ce51c1f5d05bccf7d732255b9a68bd95
David Nitu <davidnitu44@gmail.com>
2024-08-28 20:16:59 +0100

    updated the checkout to form better data for the template view

--- routes/orders.js
+++ routes/orders.js
@@ -1,23 +1,36 @@
-const express = require('express')
-const { isCartEmpty } = require('../middleware')
+const express = require('express');
+const { isCartEmpty } = require('../middleware');
+const orderSummaryView = require('../views/orders/summary');
 
-const CartRepository = require('../repositories/carts')
-const ProductsRepository = require('../repositories/products')
-const OrdersRepository = require('../repositories/orders')
+const CartRepository = require('../repositories/carts');
+const OrdersRepository = require('../repositories/orders');
+const ProductsRepository = require('../repositories/products');
 
-const router = express.Router()
+const router = express.Router();
 
 router.post('/checkout', isCartEmpty, async (req, res) => {
-  const cart = await CartRepository.getOneById(req.session.cart)
-  
+  const cart = await CartRepository.getOneById(req.session.cart);
+
   const totalPrice = cart.items.reduce((total, item) => {
-    return total+= item.quantity * item.itemPrice
-  }, 0)
+    return (total += item.quantity * item.itemPrice);
+  }, 0);
 
-  const newOrder = { items: [...cart.items ], totalPrice, date: new Date()}
+  const newOrder = { items: [...cart.items], totalPrice, date: new Date() };
+  const order = await OrdersRepository.create({ ...newOrder });
+
+  const itemsIds = order.items.map((item) => item.id);
+  const products = await ProductsRepository.getMany({ id: [...itemsIds] });
   
-  const order = await OrdersRepository.create({ ...newOrder })
-  res.redirect('/cart')
-})
+  for(let product of products) {
+    for(let item of order.items) {
+      if(item.id === product.id) {
+        item.name = product.productName
+        item.image = product.productImage
+      }
+    }
+  }
 
-module.exports = router
+  res.send(orderSummaryView({ order }));
+});
+
+module.exports = router;
