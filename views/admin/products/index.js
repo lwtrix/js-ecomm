b5edb43747d564abaf06c2f03d2eda414692e7df
David Nitu <davidnitu44@gmail.com>
2024-09-06 13:20:43 +0100

    updated admin products index view UI

--- views/admin/products/index.js
+++ views/admin/products/index.js
@@ -4,45 +4,46 @@ module.exports = ({ products }) => {
   const renderedProducts = products
     .map((product) => {
       return `
-      <tr>
-        <th scope="row">${product.id}</th>
-        <td>${product.productName}</td>
-        <td>${product.productPrice}</td>
-        <td>
+      <div class="product-row row my-1">
+        <div class="col-1 d-flex align-items-center">
+          <div class="img-container">
+            <img src="data:image/png;base64, ${product.productImage}"/>
+          </div>
+        </div>
+        <div class="col-8 d-flex align-items-center">${product.productName}</div>
+        <div class="col-1 d-flex align-items-center">£${product.productPrice}</div>
+        <div class="col-1 d-flex align-items-center">
           <a class="btn btn-success" href="/admin/products/${product.id}/edit">Edit</a>
-        </td>
-        <td>
+        </div>
+        <div class="col-1 d-flex align-items-center">
         <form method="POST" action="/admin/products/${product.id}/delete">
           <button class="btn btn-danger">Delete</button>
         </form>
-        </td>
-      </tr>
+        </div>
+      </div>
     `;
     })
     .join('');
 
   return layout({
+    page: 'products',
     content: `
-    <div class="mt-5">
-    <div class="d-flex justify-content-between align-items-center">
-      <p class="h4">Products</p>
-      <a class="btn btn-primary mt-2" href="/admin/products/add">Add Product</a>
-    </div>
-      <table class="table mt-4">
-        <thead class="table-dark">
-          <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Name</th>
-            <th scope="col">Price</th>
-            <th scope="col">Edit</th>
-            <th scope="col">Delete</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${renderedProducts}        
-        </tbody>
-      </table>
-
+    <div class="products-view">
+      <div class="d-flex justify-content-between align-items-center">
+        <p class="h4">Products</p>
+        <a class="btn btn-primary mt-2" href="/admin/products/add">Add Product</a>
+      </div>
+      
+      <div class="products-container d-flex flex-column justify-content-center">
+        <div class="table-headings row">
+          <div class="col-1">Image</div>
+          <div class="col-8">Name</div>
+          <div class="col-1">Price</div>
+          <div class="col-1">Edit</div>
+          <div class="col-1">Remove</div>
+        </div>  
+        ${renderedProducts}
+      </div>
     </div>
     `,
   });
