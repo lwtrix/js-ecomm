3db6ed92206856a8830e084e6fed7ce61ffa9ff9
David Nitu <davidnitu44@gmail.com>
2024-09-17 22:16:03 +0100

    added script for binding every custom div btn to their nearest form element

--- views/admin/layout.js
+++ views/admin/layout.js
@@ -1,3 +1,5 @@
+
+
 module.exports = ({ content, page }) => {
   return `
     <!DOCTYPE html>
@@ -38,9 +40,11 @@ module.exports = ({ content, page }) => {
             </nav>
 
             ${content}
+        
           </div>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-        </body>
+          <script defer src="/js/handleDivBtns.js"></script>
+          </body>
     </html>
   `;
 };
