9e0c619e9b3bceb7147f630ad7915682c3f885d3
David Nitu <davidnitu44@gmail.com>
2024-09-21 13:50:37 +0100

    added icons to admin dashboard stats cards + css

--- views/admin/dashboard.js
+++ views/admin/dashboard.js
@@ -10,15 +10,24 @@ module.exports = ({ stats }) => {
       <div class="stats-container">
         <div class="d-flex align-items-center">
           <div class="stat-card">
-            <div class="label">Today's Revenue</div>
+            <div class="label">
+              <i class="bi bi-wallet2"></i>
+              <span>Today's Revenue</span>
+            </div>
             <div class="value">£${stats.todaysRevenue}</div>
           </div>
           <div class="stat-card">
-            <div class="label">Today's Sales</div>
+            <div class="label">
+              <i class="bi bi-receipt"></i>
+              <span>Today's Sales</span>
+            </div>
             <div class="value">${stats.todaysSales}</div>
           </div>
           <div class="stat-card">
-            <div class="label">Total Revenue</div>
+            <div class="label">
+              <i class="bi bi-bank"></i>
+              <span>Total Revenue</span>
+            </div>
             <div class="value">£${stats.totalRevenue}</div>
           </div>
         </div>
