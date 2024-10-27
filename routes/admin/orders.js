6b1ee7b23f9873655520b2bbd447c99d1684d223
David Nitu <davidnitu44@gmail.com>
2024-10-27 10:36:45 +0000

    added admin route to GET individual order

--- routes/admin/orders.js
+++ routes/admin/orders.js
@@ -1,19 +1,29 @@
-const express = require('express')
+const express = require('express');
 
-const OrdersRepository = require('../../repositories/orders')
-const adminOrdersView = require('../../views/admin/orders/index')
+const OrdersRepository = require('../../repositories/orders');
+const adminOrdersView = require('../../views/admin/orders/index');
+const Products = require('../../repositories/products');
 
-
-const router = express.Router()
+const router = express.Router();
 
 router.get('/admin/orders', async (req, res) => {
-  const orders = await OrdersRepository.getAll()
+  const orders = await OrdersRepository.getAll();
 
-  if(!orders.length) {
-    return res.send([])
+  if (!orders.length) {
+    return res.send([]);
   }
 
-  return res.send(adminOrdersView({ orders }))
-})
+  return res.send(adminOrdersView({ orders }));
+});
 
-module.exports = router
+router.get('/admin/orders/:id', async (req, res) => {
+  const orderId = req.params.id;
+  const order = await OrdersRepository.getOneById(orderId);
+  let itemsIds = order.items.map(item => item.id);
+  console.log(itemsIds)
+  const orderItems = await Products.getMany({ id: [...itemsIds] });
+
+  res.send(orderItems)
+});
+
+module.exports = router;
