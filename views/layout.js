af6440f2f5e0baf7ad9cfc1b529df08a080af056
David Nitu <davidnitu44@gmail.com>
2024-11-30 13:01:55 +0000

    refactored main.css file into multiple for proper categorization + client nav changes

--- views/layout.js
+++ views/layout.js
@@ -4,27 +4,21 @@ module.exports = ({ content }) => {
     <html>
       <head>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;300;500;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="/styles/main.css"/>
+        <link rel="stylesheet" href="/styles/client.css"/>
         <link rel="stylesheet" href="/fonts/bootstrap-icons.css">
       </head>
         <body>
-          <nav class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
+          <nav>
             <div class="container">
-              <a class="navbar-brand" href="/"><img src="/logo/monklogo.png" width="120px"/></a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav ms-auto d-flex align-items-center">
-                  <li class="nav-item">
-                    <a class="nav-link active fs-5" aria-current="page" href="/">Shop</a>
-                  </li>
-                  <li class="nav-item ms-3">
-                    <a class="nav-link active" aria-current="page" href="/cart">
-                      <i class="bi bi-cart" style="font-size: 1.5rem"></i>
-                    </a>
-                  </li>
-                </ul>
+              <a class="nav-logo" href="/"><img src="/logo/monklogo.png"/></a>
+              <div class="nav-right">
+                <a class=""href="/cart">
+                  <i class="bi bi-cart" style="font-size: 1.5rem"></i>
+                </a>
               </div>
             </div>
           </nav>
